ab4f5b7bbaae86ade3aaef1cd1a21a725f6b02e6
Brandon Ortega <114270169+brando161@users.noreply.github.com>
2024-01-15 17:05:27 -0500

    Create function to generate random quotes

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 let apiQuotes = [];
 
+// Show New Quote
+const newQuote = () => {
+    // Pick a random quote from the apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
 // Get Quotes From API
 async function getQuotes() {
     const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
     try {
         const response = await fetch(apiUrl);
         apiQuotes = await response.json()
-        console.log(apiQuotes);
+        newQuote();
     } catch(error) {
         //Catch Error Here
     }

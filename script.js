d4ff9bb594da7cc3920f1e6d2f62080ccd61f93a
Brandon Ortega <114270169+brando161@users.noreply.github.com>
2024-01-15 16:23:06 -0500

    Create async function to fetch API

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Testing");
+let apiQuotes = [];
+
+// Get Quotes From API
+async function getQuotes() {
+    const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json()
+        console.log(apiQuotes);
+    } catch(error) {
+        //Catch Error Here
+    }
+}
+
+// On Load
+getQuotes();
